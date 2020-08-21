bfe0e9721560cbb0aad9c50e1b553507546910f1
dh.min <doohong91@gmail.com>
2020-08-21 15:30:08 +0900

    update path of saved images

--- api/models.py
+++ api/models.py
@@ -1,6 +1,38 @@
+from datetime import datetime
+import os
 from django.db import models
 
+
+def set_filename_format(now, instance, filename):
+    """ file format setting 
+     e.g)
+        {username}-{date}-{microsecond}{extension} 
+        hjh-2016-07-12-158859.png """
+    return "{username}-{date}-{microsecond}{extension}".format(
+        username=instance.user.username,
+        date=str(now.date()),
+        microsecond=now.microsecond,
+        extension=os.path.splitext(filename)[1],
+    )
+
+
+def user_directory_path(instance, filename):
+    """ image upload directory setting
+    e.g) 
+        images/{year}/{month}/{day}/{username}/{filename} 
+        images/2016/7/12/hjh/hjh-2016-07-12-158859.png """
+    now = datetime.now()
+    path = "images/{year}/{month}/{day}/{username}/{filename}".format(
+        year=now.year,
+        month=now.month,
+        day=now.day,
+        username=instance.user.username,
+        filename=set_filename_format(now, instance, filename),
+    )
+    return path
+
+
 class Image(models.Model):
-    img = models.ImageField()
+    img = models.ImageField(upload_to=user_directory_path)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$g$^kbnv#$mm)4gj%y73593nm%bm5qo-vel9ve&6exwdl=989m'
+SECRET_KEY = "$g$^kbnv#$mm)4gj%y73593nm%bm5qo-vel9ve&6exwdl=989m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -33,18 +33,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # rest-framework
-    'rest_framework',
-
+    "rest_framework",
     # apps
-    'api',
+    "api",
 ]
 
 # REST_FRAMEWORK = {
@@ -54,44 +52,62 @@ INSTALLED_APPS = [
 # }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = "backend.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3",}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'bangpl',
+    #     'USER': 'da_team',
+    #     'PASSWORD': 'alticast1!',
+    #     'HOST': '10.60.1.157',   # Or an IP Address that your database is hosted on
+    #     'PORT': '3306',
+    #     #optional:
+    #     'OPTIONS': {
+    #         'charset' : 'utf8',
+    #         'use_unicode' : True,
+    #          'init_command': 'SET '
+    #             'storage_engine=INNODB,'
+    #             'character_set_connection=utf8,'
+    #             'collation_connection=utf8_bin'
+    #             #'sql_mode=STRICT_TRANS_TABLES,'    # see note below
+    #             #'SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',
+    #     },
+    #     'TEST_CHARSET': 'utf8',
+    #     'TEST_COLLATION': 'utf8_general_ci',
+    # }
 }
 
 
@@ -99,27 +115,19 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
@@ -131,10 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # Media files
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")

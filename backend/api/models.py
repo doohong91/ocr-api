803d1929704c7c265e7c6d4d59fffc033e1bd08a
dh.min <doohong91@gmail.com>
2020-09-01 15:28:59 +0900

    add imageFields for original image and result image & jsonfield for bounding box

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,23 +1,28 @@
 from datetime import datetime
 import os
+
 from django.db import models
 
 
 def set_filename_format(now, instance, filename):
-    """ file format setting 
-     e.g)
-        {username}-{date}-{microsecond}{extension} 
-        hjh-2016-07-12-158859.png """
+    """ 
+    file format setting 
+    e.g)
+        {date}-{microsecond}{extension} 
+        2020-08-19-158859.png 
+    """
     return "{date}-{microsecond}{extension}".format(
         date=str(now.date()), microsecond=now.microsecond, extension=os.path.splitext(filename)[1],
     )
 
 
 def user_directory_path(instance, filename):
-    """ image upload directory setting
+    """ 
+    image upload directory setting
     e.g) 
-        images/{year}/{month}/{day}/{username}/{filename} 
-        images/2016/7/12/hjh/hjh-2016-07-12-158859.png """
+        images/{year}/{month}/{day}/{filename} 
+        images/2020/8/19/2020-08-19-158859.png 
+    """
     now = datetime.now()
     path = "images/{year}/{month}/{day}/{filename}".format(
         year=now.year,
@@ -29,6 +34,8 @@ def user_directory_path(instance, filename):
 
 
 class Image(models.Model):
-    img = models.ImageField(upload_to=user_directory_path)
+    original_img = models.ImageField(upload_to=user_directory_path)
+    result_img = models.ImageField(upload_to=user_directory_path)
+    bounding_boxes = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

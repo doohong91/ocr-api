7870301214db05f25d7d6b2d37205839783c33ea
dh.min <doohong91@gmail.com>
2020-09-10 10:58:13 +0900

    add engines.py: call clova ocr engine

--- backend/plugins/engines.py
+++ backend/plugins/engines.py
@@ -1,1 +1,29 @@
+import requests
+import time
 
+
+def send(file, URL, SECRET_KEY):
+	name = file.name.split(".")[0]
+	timestamp = time.time()
+	requestId = f'{file.content_type.split("/")[0]}_{timestamp}'
+
+	message = {
+		"requestId": requestId,
+		"version": "V2",
+		"images": [{
+			"name": name,
+			"format": file.content_type.split("/")[1]
+		}],
+		"timestamp": str(timestamp)
+	}
+
+    payload = {"message": str(message)}
+
+    headers = {"Content-Type": "multipart/form-data", "X-OCR-SECRET": SECRET_KEY}
+
+    response = requests.request("POST", URL, headers=headers, data=payload, files=[file])
+
+    image = response.json().get("image")
+    if image:
+        return image.values[0].get("fields")
+    return None
